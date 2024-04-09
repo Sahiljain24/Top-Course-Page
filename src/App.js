874a64ac0d756e36fd3a086263bc005b0cbb661c
Sahiljain24 <24sahilj@gmail.com>
2024-04-09 23:25:06 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cards from "./components/Cards";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+const App = () => {
+  return <div>
+    <h1>hello</h1>
+    <Navbar/>
+    <Filter/>
+    <Cards/>
+  </div>;
+};
+
+export default App;
